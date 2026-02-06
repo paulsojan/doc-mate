31f89be2a77bb040a79202e7f58b7b62d814982a
Paul Elias Sojan <paul.eliassojan@gmail.com>
2026-02-06 23:02:28 +0530

    Refactor Dashboard index to improve layout structure and semantic HTML

--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -3,20 +3,22 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import PageNotFound from "components/commons/PageNotFound";
-import Sidebar from "components/Dashboard/Sidebar";
 import { DASHBOARD_ROUTES } from "components/routeConstants";
 
+import Sidebar from "./Sidebar";
+
 const Dashboard = () => (
-  <div className="flex">
+  <div className="flex h-screen w-full overflow-hidden">
     <Sidebar />
-    <div className="w-full">
+    <main className="flex-grow overflow-y-auto">
       <Switch>
         {DASHBOARD_ROUTES.map(({ path, component }) => (
           <Route exact component={component} key={path} path={path} />
         ))}
         <Route component={PageNotFound} path="*" />
       </Switch>
-    </div>
+    </main>
   </div>
 );
+
 export default Dashboard;
