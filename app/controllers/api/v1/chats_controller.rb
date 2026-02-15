64e90636453e380465f698bf155523cde9b14bcc
Paul Elias Sojan <paul.eliassojan@gmail.com>
2026-02-15 15:06:44 +0530

    Refactor ChatsController: use ordered scope, load_chat! for question action, and fix indentation

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Api::V1::ChatsController < Api::V1::BaseController
-  before_action :load_chat!, only: :show
+  before_action :load_chat!, only: %i[show question]
 
   def index
-    @chats = current_user.chats.all.order(created_at: :desc)
+    @chats = current_user.chats.ordered
   end
 
   def show
@@ -12,19 +12,19 @@ class Api::V1::ChatsController < Api::V1::BaseController
   end
 
   def question
-    answer = QuestionAnswerService.new(question_answer_params[:question], params[:id]).process
+    answer = QuestionAnswerService.new(question_params[:question], @chat).process
     render_json({ answer: })
   end
 
   private
 
-    def load_chat!
-      @chat = current_user.chats.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render_error(t("chat.invalid"))
-    end
+  def load_chat!
+    @chat = current_user.chats.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render_error(t("chat.invalid"))
+  end
 
-    def question_answer_params
-      params.require(:chats).permit(:question)
-    end
+  def question_params
+    params.require(:chats).permit(:question)
+  end
 end
