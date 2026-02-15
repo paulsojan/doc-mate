f6955e0635c8f0ad50c8dbe0bdd9631179d9b56d
Paul Elias Sojan <paul.eliassojan@gmail.com>
2026-02-15 16:31:55 +0530

    Refactor Api::V1::ChatsController for improved readability and best practices

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Api::V1::ChatsController < Api::V1::BaseController
-  before_action :load_chat!, only: :show
+  before_action :set_chat, only: :show
 
   def index
-    @chats = current_user.chats.all.order(created_at: :desc)
+    @chats = current_user.chats.order(created_at: :desc)
+    render_json(@chats)
   end
 
   def show
@@ -12,19 +13,20 @@ class Api::V1::ChatsController < Api::V1::BaseController
   end
 
   def question
-    answer = QuestionAnswerService.new(question_answer_params[:question], params[:id]).process
-    render_json({ answer: })
+    answer = QuestionAnswerService
+               .new(question_answer_params[:question], params[:id])
+               .process
+    render_json(answer: answer)
   end
 
   private
 
-    def load_chat!
-      @chat = current_user.chats.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render_error(t("chat.invalid"))
-    end
+  def set_chat
+    @chat = current_user.chats.find_by(id: params[:id])
+    render_error(t('chat.invalid')) unless @chat
+  end
 
-    def question_answer_params
-      params.require(:chats).permit(:question)
-    end
+  def question_answer_params
+    params.require(:chats).permit(:question)
+  end
 end
